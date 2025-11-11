45a2d94a810fb4b70add2c019990f2e62b2381ba
meron847 <belachewmeron15@gmail.com>
2025-11-11 18:02:47 +0000

    use if and else if

--- experments.js
+++ experments.js
@@ -2,3 +2,22 @@ const monthlyRent = 600;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
+
+
+const skyIsBlue = true;
+
+if (skyIsBlue) {
+ console.log("The sky is blue!");
+}else {
+    console.log("The sky is ...not blue");
+}
+
+const friendsAtYourParty = 0;
+
+if (friendsAtYourParty=== 0) {
+    console.log("Cool.");
+}else if (friendsAtYourParty < 4) {
+    console.log("perfect amount.");
+} else {
+    console.log("wooooooooo")
+}
