86ff4ba4f0c3ac7392be44b628f24774c0503e45
meron847 <belachewmeron15@gmail.com>
2025-11-11 18:51:36 +0000

    while loop

--- experments.js
+++ experments.js
@@ -12,12 +12,20 @@ if (skyIsBlue) {
     console.log("The sky is ...not blue");
 }
 
-const friendsAtYourParty = 0;
+const friendsAtYourParty = 0;/*you can have multiple type of branching methode  */
 
 if (friendsAtYourParty=== 0) {
     console.log("Cool.");
-}else if (friendsAtYourParty < 4) {
+}else if (friendsAtYourParty <= 4) {
     console.log("perfect amount.");
 } else {
-    console.log("wooooooooo")
+    console.log("wooooooooo")/*if nothing is true run this  */
 }
+
+let friends = 0;
+
+while (friends < 10) {
+    console.log("another friend");
+    friends = friends + 2;
+}
+console.log(friends);
