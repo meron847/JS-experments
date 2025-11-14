8ae2f33c88e9fff248bc411262f426a81666dd4c
meron847 <belachewmeron15@gmail.com>
2025-11-14 17:07:58 +0000

    comment out loop

--- experments.js
+++ experments.js
@@ -1,5 +1,5 @@
 let friends =0;
-for (let i= 0; i< 1000; i++)/*for loop have 3 section the first on is declaration and 2 one is the while part(i is < 10 do some thing ) 3 one is  do some thing on the variable usually i++ */ {
+for (let i= 0; i< 1000; i++)/*for loop have 3 section the first on is declaration and 2 one is the while part(i is < 10 do some thing ) 3 one is increment/decrement of the control variable at the end of each iteration */ {
     friends++;
 }
 console.log(friends);
