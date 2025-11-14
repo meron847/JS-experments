2c8da3dab6153a65b899aaf852053d55e429d72c
meron847 <belachewmeron15@gmail.com>
2025-11-14 17:00:53 +0000

    for loop

--- experments.js
+++ experments.js
@@ -1,31 +1,9 @@
-const monthlyRent = 600;
-
-const yearlyRent = monthlyRent * 12;
-console.log(yearlyRent);
-
-
-const skyIsBlue = true;
-
-if (skyIsBlue) {
- console.log("The sky is blue!");
-}else {
-    console.log("The sky is ...not blue");
-}
-
-const friendsAtYourParty = 0;/*you can have multiple type of branching methode  */
-
-if (friendsAtYourParty=== 0) {
-    console.log("Cool.");
-}else if (friendsAtYourParty <= 4) {
-    console.log("perfect amount.");
-} else {
-    console.log("wooooooooo")/*if nothing is true run this  */
-}
-
-let friends = 0;
-
-while (friends < 10) {
-    console.log("another friend");
-    friends = friends + 2;
+let friends =0;
+for (let i= 0; i< 1000; i++)/*for loop have 3 section the first on is declaration and 2 one is the while part(i is < 10 do some thing ) 3 one is  do some thing on the variable usually i++ */ {
+    friends++;
 }
 console.log(friends);
+
+
+
+
