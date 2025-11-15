70402a978d9f18409849dd62e25b11f66740344c
meron847 <belachewmeron15@gmail.com>
2025-11-15 11:22:08 +0000

    function

--- experments.js
+++ experments.js
@@ -24,5 +24,27 @@ const main = '$'
 console.log("".padStart(repeatedTimes,main)); //used to quickly create a repeated character string//
 
 
+function addTwo(number){
+    return number + 2;
+}
+
+const finalAnswer = addTwo(5);
+console.log(finalAnswer);
+
+const otherAnswer = addTwo(10);
+console.log(otherAnswer);
+
+
+
+const myHome = "Seattle";
+const myState = "Washington";
+const myCountry = "USA";
+
+function logOutYourHome(home, state, Country) {
+    console.log(`You are from ${home}, ${state}, ${Country}.`);
+}
+
+logOutYourHome(myHome,myCountry,myState);
+
 
 
